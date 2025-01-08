7614f91911a0c88f13a65824ec7857ded6eb070d
Ghostified <bransonallan@gmail.com>
2025-01-09 01:32:38 +0300

    guess

--- guessWhile.c
+++ guessWhile.c
@@ -5,13 +5,26 @@ int main(){
 
   int secret = 5;
   int guess;
+  int guessLimit = 3;
+  int guessCount = 0;
+  int outOfGuesses = 0; 
 
-  while( guess != secret){
-    printf("Enter a number: ");
-    scanf("%d" , &guess);
-    printf("Try again\n");
+  while( guess != secret && outOfGuesses == 0){
+    if (guessCount < guessLimit) {
+      printf("Enter a number: ");
+      scanf("%d", &guess);
+      guessCount++;
+    } else {
+      outOfGuesses = 1;
+      
+    }
+    
   }
-  printf("You win");
+  if (outOfGuesses == 1){
+    printf("Out of guesses, you Loose \n");
+  } else {
+    printf("You win");
+  }
 
 
   return 0; 
